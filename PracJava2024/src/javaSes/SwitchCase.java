295d1557c6ad5847873b2eab6091f651749a69cc
s-sn-s <savin.sn.shetty@gmail.com>
2025-04-03 22:54:07 +0530

    switch case explore

---
+++ PracJava2024/src/javaSes/SwitchCase.java
@@ -0,0 +1,26 @@
+package javaSes;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    char switchMe = 's';
+
+        switch(switchMe){
+            case 'a':
+                System.out.println("Test A");
+                break;
+            case 's':
+                System.out.println("Test B");
+                break;
+            case 'c':
+                System.out.println("Test C");
+                break;
+            default:
+                System.out.println("Test Default");
+                break;
+        }
+
+	}
+
+}
