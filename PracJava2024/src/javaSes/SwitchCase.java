d44fd6989837521e2eec164c4fd6d81a949ca37b
s-sn-s <savin.sn.shetty@gmail.com>
2025-04-06 22:30:28 +0530

    switch me

--- PracJava2024/src/javaSes/SwitchCase.java
+++ PracJava2024/src/javaSes/SwitchCase.java
@@ -3,8 +3,11 @@ package javaSes;
 public class SwitchCase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	    char switchMe = 's';
+		
+		SwitchCase sc = new SwitchCase();
+		char switchMe = 's';
+		
+		if(sc.checkMyState(switchMe)) {
 
         switch(switchMe){
             case 'a':
@@ -19,8 +22,23 @@ public class SwitchCase {
             default:
                 System.out.println("Test Default");
                 break;
-        }
-
+        	}
+		}
+	}
+	
+	private Boolean checkMyState(char c) {
+		
+		switch(c) {
+		case 'x':
+			System.out.println("x");
+			return true;
+		case 'y':
+			System.out.println("y");
+			return false;
+		default: 
+			System.out.println("really?");
+			return true;
+		}		
 	}
 
 }
