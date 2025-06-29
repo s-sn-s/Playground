34fd381d1c81f4ca540422d5afe4e8f5d52323cf
s-sn-s <savin.sn.shetty@gmail.com>
2025-06-29 23:07:22 +0530

    Test Connect

---
+++ QuickSel/src/main/java/SelConnect/Login.java
@@ -0,0 +1,27 @@
+package SelConnect;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login{
+	
+	public static void main(String[] args) throws InterruptedException{
+		
+		String siteName = "https://naveenautomationlabs.com/opencart/index.php?route=account/login";
+		
+		WebDriver d = new ChromeDriver();
+		
+		d.manage().window().maximize();
+		
+		System.out.println("session init'ed!");
+		
+		d.get(siteName);
+		
+		System.out.println(d.getTitle());
+//		d.wait(5000);
+		
+		d.quit();
+		
+	}
+	
+}
