9787e66488c8c54c78656e785b3c9d2dc17c9a38
s-sn-s <savin.sn.shetty@gmail.com>
2025-07-09 23:30:28 +0530

    jaSelLecture20250709

--- PracJava2024/src/revision/myDataTypes.java
+++ PracJava2024/src/revision/myDataTypes.java
@@ -8,11 +8,22 @@ public class myDataTypes{
 		
 		System.out.println(b);
 		
-		//bit -> byte -> short -> int -> long
-		//float -> double
-		//boolean
-		//char
-		//String
+		/*
+		byte 		- 1Byte	= 8bit
+		short 	- 2B	= 16b
+		int		- 4B	= 32b
+		long		- 8B	= 64b
+		float		- 4B	= 32b
+		double	- 8B	= 64b
+		boolean	- ~1b
+		char		- 2B
+		char should be written inside a single quote
+		char are from numeric family 
+		so if any mathematical operation is performed 
+		it results in ASCII value of char used
+		String
+		String should be written inside a double quote
+		*/
 		
 		System.out.println("Hello " + "World!!");
 		
