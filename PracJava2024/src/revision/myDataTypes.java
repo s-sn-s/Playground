5cc63e62b0f56dcfe3cc128c6b2510c8cd05199f
s-sn-s <savin.sn.shetty@gmail.com>
2025-07-09 23:28:15 +0530

    jaSelLecture20250709

---
+++ PracJava2024/src/revision/myDataTypes.java
@@ -0,0 +1,21 @@
+package revision;
+
+public class myDataTypes{
+	
+	public static void main(String[] args) {
+		
+		byte b = 1;
+		
+		System.out.println(b);
+		
+		//bit -> byte -> short -> int -> long
+		//float -> double
+		//boolean
+		//char
+		//String
+		
+		System.out.println("Hello " + "World!!");
+		
+		
+	}
+}
